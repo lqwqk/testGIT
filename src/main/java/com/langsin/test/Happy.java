b6af17882cdad3b1e5f298a99033294b0448c3a6
lqwqk_glb <lqwqk_glb@126.com>
2019-04-26 14:14:18 +0800

    解决 冲突 left and  right

--- src/main/java/com/langsin/test/Happy.java
+++ src/main/java/com/langsin/test/Happy.java
@@ -2,6 +2,7 @@ package com.langsin.test;
 
 public class Happy {
 	public static void main(String[] args) {
-		System.out.println("456");
+		System.out.println("456 left");
+		System.out.println("456 right");
 	}
 }
