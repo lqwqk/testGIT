eafb02ee04b8412925815d58c88079440157b21b
lqwqk_glb <lqwqk_glb@126.com>
2019-04-26 16:32:53 +0800

    Idea 新增分支 和 新加入一个打印语句!

--- src/main/java/com/langsin/test/Happy.java
+++ src/main/java/com/langsin/test/Happy.java
@@ -2,6 +2,7 @@ package com.langsin.test;
 
 public class Happy {
 	public static void main(String[] args) {
+		System.out.println("idea 添加新分支 并修改内容");
 		System.out.println("456 left");
 		System.out.println("456 right");
 		System.out.println("合并");
